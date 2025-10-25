cdee0b3b7e793739a2caacf052436e91e5c2eb1a
Phahose <ekwomnick@gmail.com>
2025-10-25 13:22:36 -0600

    The Visual Layout of the App done now onto the functionality

--- Speko.Overlay/MainWindow.xaml.cs
+++ Speko.Overlay/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -6,7 +10,6 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Speko.Overlay
@@ -21,14 +24,23 @@ namespace Speko.Overlay
             InitializeComponent();
         }
 
-        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        private void StartMeeting_Click(object sender, RoutedEventArgs e)
         {
-            if (e.LeftButton == MouseButtonState.Pressed)
-            {
-                this.DragMove();
-            }
-                
+            // Create and show the overlay window
+            MeetingMode meetingMode = new MeetingMode();
+            meetingMode.Show();
+
+            // Hide (not close) the main dashboard
+            this.Hide();
         }
 
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Position the overlay at the top center of the screen
+            double screenWidth = SystemParameters.PrimaryScreenWidth;
+            double windowWidth = this.Width;
+            this.Left = (screenWidth - windowWidth) / 2; // center horizontally
+            this.Top = 20; // 20 pixels from top edge
+        }
     }
 }
---
+++ Speko.Overlay/MeetingMode.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Speko.Overlay
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MeetingMode : Window
+    {
+        public MeetingMode()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                this.DragMove();
+            }
+                
+        }
+
+    }
+}
